b89b269181f50557cf0c7518ff272d04cb38b07e
DY <23586296+Kompensator@users.noreply.github.com>
2019-03-02 21:26:31 -0500

    another useless comment

--- main.py
+++ main.py
@@ -7,13 +7,8 @@ width = 800
 height = 600
 bg_colour = (100, 100, 100)
 fps = 60
-<<<<<<< Updated upstream
-secs_per_msecs = 1e5
-pixels_per_meter = (1/3*height)/1.5e11
-=======
 secs_per_msecs = 3.6e5
 pixels_per_meter = (1/4*height)/1.5e11
->>>>>>> Stashed changes
 body_scale = 1000
 
 #Conversion from pixels to meters
